847246d4716a891ffba55faba2c340511d14a55e
Yelllowww <eduardodovale197@gmail.com>
2026-02-24 04:49:14 -0300

    loop de execução

--- main.cs
+++ main.cs
@@ -4,27 +4,38 @@ using System.Text.Json.Serialization;
 Console.Title = "RPGProject";
 //Console.BackgroundColor = ConsoleColor.DarkRed;
 //Console.ForegroundColor = ConsoleColor.Black;
-Console.Clear();
 var json = File.ReadAllText("agentes.json");
 var agentes = JsonSerializer.Deserialize<Dictionary<string, Agente>>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
 ) ?? new Dictionary<string, Agente>(StringComparer.OrdinalIgnoreCase);
+while (true) {
+    Console.Clear();
+    Console.WriteLine("----------RPGProject menu----------\n");
+    Console.WriteLine($"Escolha um agente: {string.Join(", ", agentes.Keys)}\n");
+    string input = Console.ReadLine();
+    if (input == "0") {break;}
+    if (agentes.TryGetValue(input, out var agente))
+    {
+        Console.Clear();
+        Console.WriteLine($"{input}:\n");
+        Console.WriteLine($"Pontos de vida: {agente.PV}");    
+        Console.WriteLine($"Sanidade: {agente.Sanidade}");
+        Console.WriteLine($"Pontos de esforço: {agente.PE}");
+        Console.WriteLine($"Defesa: {agente.Defesa}");
+        Console.WriteLine($"Esquiva: {agente.Esquiva}");
+        Console.WriteLine("\nPressione Enter para voltar ao menu...");
+        string exit = Console.ReadLine();
+        if (exit == "0") {break;}
 
-Console.WriteLine("----------RPGProject menu----------\n");
-Console.WriteLine($"Escolha um agente: {string.Join(", ", agentes.Keys)}");
-string input = Console.ReadLine();
-if (agentes.TryGetValue(input, out var agente))
-{
-    Console.WriteLine($"{input}:\n");
-    Console.WriteLine($"Pontos de vida: {agente.PV}");    
-    Console.WriteLine($"Sanidade: {agente.Sanidade}");
-    Console.WriteLine($"Pontos de esforço: {agente.PE}");
-    Console.WriteLine($"Defesa: {agente.Defesa}");
-    Console.WriteLine($"Esquiva: {agente.Esquiva}");
-}
-else
-{
-    Console.WriteLine("Nome incorreto/não existente");
-}
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Nome incorreto/não existente");
+        Console.WriteLine("\nPressione Enter para voltar ao menu...");
+        string exit = Console.ReadLine();
+        if (exit == "0") {break;}
+    }
+} 
 class Agente
 {
     public string Nome { get; set; } = "";
